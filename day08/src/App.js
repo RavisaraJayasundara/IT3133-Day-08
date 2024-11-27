f4266e07e632bcf71acd807dbd6e034bc846ab4d
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-27 18:26:52 +0530

    Edit app.js using router commands

--- day08/src/App.js
+++ day08/src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
+import {BrowserRouter as Router,Routes,Route,NavLink} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-       <Home/>
-       <AboutUs/>
-       <ContactUs/>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/home" element={<AboutUs/>}/>
+          <Route path="/home" element={<ContactUs/>}/>
+
+        </Routes>
+      </Router>
+       </div>
   );
 }
 
