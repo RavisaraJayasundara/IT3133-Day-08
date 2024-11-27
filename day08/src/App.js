bc78895b4b46e9ce20c96fd977210de2acc115c2
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-27 18:30:17 +0530

    add Links

--- day08/src/App.js
+++ day08/src/App.js
@@ -2,17 +2,24 @@ import './App.css';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
-import {BrowserRouter as Router,Routes,Route,NavLink} from 'react-router-dom'
+import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/about">AboutUs</Link></li>
+            <li><Link to="/contact">ContactUs</Link></li>
+          </ul>
+        </nav>
         <Routes>
           <Route path="/home" element={<Home/>}/>
-          <Route path="/home" element={<AboutUs/>}/>
-          <Route path="/home" element={<ContactUs/>}/>
+          <Route path="/about" element={<AboutUs/>}/>
+          <Route path="/contact" element={<ContactUs/>}/>
 
         </Routes>
       </Router>
